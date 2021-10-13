a99f8303fc803329bf5014d70f691b05b6a97e47
xDRUI22 <40361819+xDRUI22@users.noreply.github.com>
2021-10-13 12:11:01 +0200

    Update codeScript.js

--- codeScript.js
+++ codeScript.js
@@ -1,5 +1,13 @@
-function msg(){  
-    //alert("Hello Javatpoint"); 
-    let value = element.getAttribute(keyWord);
-    alert(value);  
+function mainFunction(){  
+    console.log('mainFunction()');
+    let keywrd = document.getElementById('keyWord').value;
+
+    if(typeof(keywrd) == 'undefined' || keywrd == null || keywrd == '') { 
+        console.log('undefined');
+        alert('Indique un texto a buscar');
+    } else {
+        console.log(keywrd);
+        alert(keywrd);
+    }
+      
 }
